993207c17cc9fd3589bac6253f44123393cedf78
Sruthi417 <sruthyy345@gmail.com>
2026-03-23 18:20:27 +0530

    api change

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Backend API base URL
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
